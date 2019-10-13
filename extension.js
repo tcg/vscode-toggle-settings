2c8f02e96bf5e71033a7069c73c04a6f0fcd75ff
tcg <tommy.george@youversion.com>
2019-10-12 20:46:21 -0500

    WIP

--- extension.js
+++ extension.js
@@ -13,31 +13,53 @@ function activate(context) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
 
+    // Get our previous storage state:
+    const globalState = context.globalState; // .get("toggleSettingsChanges.state", {})
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.main', () => {
+	let disposable = vscode.commands.registerCommand('extension.main', (globalState) => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
         // vscode.window.showInformationMessage('Hello World!');
-        main()
+        main(globalState)
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 
-function main() {
+/**
+ * Returns `true` if the values stored in the Config/Settings for this Extension are the same as the key/values provided in the given `settingsObject` Object. Otherwise, `false`.
+ *
+ * @param {object} settingsObject - An object of settings values to compare with what's stored in the settings of this extension.
+ */
+function settingsAreSameAsDefined(settingsObject) {
+    // TODO: Not sure how best to implement this yet.
+    // I'm so accustomed to Python object comparisons, I'm not 100% sure on this.
+}
+
+/**
+ *
+ * @param {object} state - Return value of ExtensionContext.globalState; used to store/retrieve values.
+ */
+function main(state) {
     // Get the config values object for our Extension (actually, empty string === ALL):
     const config = vscode.workspace.getConfiguration('', {});
-
+    const oldState = state.get("toggleSettingsChanges.state", {});
     // const values = config.get('toggleSettingsChanges.settingsToToggle', {});
     // const values = config.get('toggleSettingsChanges.settingsToToggle', {});
     const values = config.get('toggleSettingsChanges.settingsToToggle', {});
-    const settingsAreToggled = config.get('toggleSettingsChanges.settingsAreToggled', false);
     if (settingsAreToggled === false) {
+        // const settingsAreToggled = config.get('toggleSettingsChanges.settingsAreToggled', false);
+        const settingsAreToggled = state.get('toggleSettingsChanges.settingsAreToggled', false);
         console.log("TODO: STORE EXISTING SETTINGS SOMEWHERE");
+    }
+    if (oldState) {
+        console.log("Previous State values discovered.");
+        console.log(oldState)
     }
    /**/
     // STREAMING SETTINGS.
@@ -61,11 +83,6 @@ function main() {
             // config.update(key, values[key])
         }
     );
-    // const results = settingsNames.map((name)=>{
-    //     console.log("Getting value for name:", name);
-    //     const existingSettingValue = config.get(name, '')
-    //     console.log(`${name}: ${existingSettingValue}`)
-    // });
 
     vscode.window.showInformationMessage('Hello MAIN!');
 }
